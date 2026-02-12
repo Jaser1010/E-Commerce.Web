6e1fcc50b8f42ce63dd22d1cd046a385a8ac21b2
Jaser1010 <j.osama2101@nu.edu.eg>
2026-02-12 11:36:55 +0200

    CustomerBasket

---
+++ E Commerce.Domain/Entities/BasketMudule/CustomerBasket.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce.Domain.Entities.BasketMudule
+{
+	public class CustomerBasket
+	{
+		public string Id { get; set; } = default!; // Guid : Created From Client Side [Frontend]
+		public ICollection<BasketItem> Items { get; set; } = [];
+	}
+}
